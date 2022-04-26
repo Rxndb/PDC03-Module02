e3458c098cb11cc4e61d7369a5e98317f56358e8
Rxndb <104422620+Rxndb@users.noreply.github.com>
2022-04-26 17:28:11 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDC03_MOD02
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void OnNextPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity1());
+        }
+        private async void OnNextPageII(Object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity2());
+        }
+    }
+}
